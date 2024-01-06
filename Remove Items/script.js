8ec32e4611d61d66c6b605fce9244efb4f625925
Sonava <sonava@code.edu.az>
2024-01-06 22:56:15 +0400

    Remove Items Added

---
+++ Remove Items/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const array = [1, 2, 2, 3, 4, 4, 4, 4, 5, 6];
+const arrayItem = 4;
+
+function removeItems(array, arrayItem) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] === arrayItem) {
+        array.splice(i, 1);
+      }
+    }
+    console.log(array);
+  }
+}
+
+removeItems(array, arrayItem);
